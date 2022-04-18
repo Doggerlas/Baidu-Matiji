3997b1664956701690cda0ad7e6d0efb71b5042d
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-18 22:09:37 +0800

    Create 4.cpp

---
+++ 4.18_4.25_Week2/4.cpp
@@ -0,0 +1,11 @@
+//小数精度及左右对齐
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    printf("%-14.6f %14.6f\n",3.1415926,3.1415926);
+    printf("%-14.6f %14.6f\n",22.3456,22.3456);
+    return 0;
+}
