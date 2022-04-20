c4116c326120532d186c00a59d7d416b23d83eb0
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 18:03:03 +0800

    Create 13.cpp

---
+++ 4.11_4.18_Week1/13.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    printf("Size of int:%d bytes\n",sizeof(int));
+    printf("Size of long int:%d bytes\n",sizeof(long int));
+    printf("Size of long long int:%d bytes\n",sizeof(long long int));
+    printf("Size of double:%d bytes\n",sizeof(double)); 
+    printf("Size of long double:%d bytes\n",sizeof(long double));
+    return 0;
+}
