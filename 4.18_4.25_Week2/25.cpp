068fec766c5813586561a65c1712b247acf55fff
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 21:15:34 +0800

    Create 25.cpp

---
+++ 4.18_4.25_Week2/25.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h> 
+//八进制十六进制ox前加# 变为以0开头
+using namespace std;
+
+int main( )
+{
+    printf("%d %#o %#X\n",202,202,202);
+    printf("%d %#o %#X\n",117,117,117);
+    printf("%d %#o %#X\n",70,70,70);
+    printf("%d %#o %#X\n",130,130,130);
+
+
+    return 0;
+}
