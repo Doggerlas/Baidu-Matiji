880b47bdc746c393d01f3c49484c528269fc9205
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 17:44:21 +0800

    Create 10.cpp

---
+++ 4.11_4.18_Week1/10.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    char a,b;
+    scanf("%c,%c",&a,&b);
+    printf("The ASCII code of %c is %d\n",a,a);
+    printf("The ASCII code of %c is %d\n",b,b); 
+    return 0;
+}
