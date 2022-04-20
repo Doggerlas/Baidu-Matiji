a87cfdfa39f8f8b71e536c123f85326beb22f83f
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 16:17:42 +0800

    Create 7.cpp

---
+++ 4.11_4.18_Week1/7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+     double a , b ,c;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    printf("%lf\n",a+b+c);
+    printf("%lf\n",(a+b+c)/3);
+    return 0;
+}
