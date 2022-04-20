3cd33a851e3e2430ba6f070116c91fc21b555c7a
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 17:59:31 +0800

    Create 12.cpp

---
+++ 4.11_4.18_Week1/12.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    printf("Size of int:%d bytes\n",sizeof(int));
+    printf("Size of float:%d bytes\n",sizeof(float));
+    printf("Size of double:%d bytes\n",sizeof(double));
+    printf("Size of char:%d bytes\n",sizeof(char)); 
+    return 0;
+}
