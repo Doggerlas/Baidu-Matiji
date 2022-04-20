3b015fcc4e3449664d267fe6fa9a2aa6c6a43aa9
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 16:13:17 +0800

    Create 6.cpp

---
+++ 4.11_4.18_Week1/6.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    double a , b;
+    scanf("%lf %lf",&a,&b);
+    printf("%lf*%lf=%lf\n",a,b,a*b);
+    printf("%lf/%lf=%lf\n",a,b,a/b);
+    return 0;
+}
