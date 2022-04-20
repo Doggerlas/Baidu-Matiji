6c5e3484904f832757eaf04d00040c20ce4d8530
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 21:01:28 +0800

    Create 23.cpp

---
+++ 4.18_4.25_Week2/23.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+//%o八进制 %x十六进制 %u无符号十进制
+//cout<<hex<<a十六进制 cout<<oct<<a八进制 cout<<dec<<a十进制
+using namespace std;
+
+int main( )
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d %o %x %u",n,n,n,n);
+    return 0;
+}
