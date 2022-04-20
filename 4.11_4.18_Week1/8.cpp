bf6e958cdec24b4ec91f854e60db73ac468a8f69
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 16:33:50 +0800

    Update 8.cpp

--- 4.11_4.18_Week1/8.cpp
+++ 4.11_4.18_Week1/8.cpp
@@ -11,6 +11,7 @@ int main( )
     printf("%.2lf\n",PI*r*r);
     printf("%.2lf\n",4*PI*r*r);
     printf("%.2lf\n",4.0/3*PI*r*r*r);
+    //这里要是不写4.0，4/3=1，因为4和3都是int，结果也只能是int。当c++运算符两边数据类型不一样时，会自动将低类型转换为高类型。所以4.0/3其实是两个float计算，结果也是float1.333
     printf("%.2lf\n",h*PI*r*r);
     return 0;
 }
