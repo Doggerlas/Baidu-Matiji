f0f7077f750c5524df5074540480380544ea5c26
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-20 15:58:03 +0800

    Create 5.cpp

---
+++ 4.11_4.18_Week1/5.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+   float a;
+   double b;
+   scanf("%f %lf",&a,&b);
+   printf("You entered:%.2f and %.3lf\n",a,b);
+   return 0;
+}
