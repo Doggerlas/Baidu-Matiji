29033a08b157ce823c27c05372ce7ae97cea8d65
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-21 18:42:42 +0800

    Create 37.cpp

---
+++ 4.18_4.25_Week2/37.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    double a;
+    scanf("%lf",&a);
+    //if(a<=0)
+    //    a=-a;
+    printf("%.6lf",fabs(a));
+    return 0;
+}
