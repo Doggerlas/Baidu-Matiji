78c3f5e1750cfb0594f27d485edc23da662adccc
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-21 18:49:25 +0800

    Create 38.cpp

---
+++ 4.18_4.25_Week2/38.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    int a,b,tep;
+    scanf("%d %d",&a,&b);
+    tep=b;
+    b=a;
+    a=tep;
+    printf("%d %d",a,b);
+    return 0;
+}
