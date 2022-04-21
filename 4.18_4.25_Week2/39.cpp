ec5e210c7fcfc0c8be09cebf2df8c7cd1f8025a3
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-21 18:55:58 +0800

    Create 39.cpp

---
+++ 4.18_4.25_Week2/39.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    int a;
+    cin>>a;
+    int id=0,ch=1,temp=a;
+    while(temp){
+        if(id%2==0&&temp%2==1){
+            a=a-ch;
+        }
+        ch*=2;
+        id++;
+        temp/=2;
+    }
+    cout<<a;
+    return 0;
+}
