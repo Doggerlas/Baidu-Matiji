6f7c06f4f10b301bc0f673f5cf1d6b945af54cf6
Wang Limeng <94960048+Doggerlas@users.noreply.github.com>
2022-04-27 18:52:51 +0800

    Create 62.cpp

---
+++ 4.25_5.09_Week3&4/62.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main( )
+{
+    int n,sum;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    sum+=i+(n/i);
+    printf("%d",sum);
+    return 0;
+}
